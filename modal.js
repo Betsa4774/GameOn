180879e48b87b6e43975282e0a22bef5eec4827d
betsa4774 <betsabe.poudevigne@gmail.com>
2021-07-09 19:46:49 +0200

    inputModal

--- modal.js
+++ modal.js
@@ -21,12 +21,11 @@ function launchModal() {
 }
 
 //ici commence l'excercise appelé par id sur le form)
- reserve = document.getElementById('reserve'); //on fait reserve et on rajoute un événement et 
-//serà à la sumision de formulaire qu on voudra declencher une fonction et cette fonction on va lui passer l'événement en question
+
+ reserve = document.getElementById('reserve'); 
  
 reserve.addEventListener('submit',function(e) {
-  // Variables--//creation des variables et recuperations d'elles mêmes par document.get et son Id.
-  // ceci permet de récuperer le formulaire
+  // Variables
   e.preventDefault();
   const first = document.getElementById('first'); 
   const last = document.getElementById('last');
@@ -38,15 +37,16 @@ reserve.addEventListener('submit',function(e) {
   const myError2 = document.getElementById('error2');
   const myError3 = document.getElementById('error3');
   const myError4 = document.getElementById('error4');
- // const location1 = document.getElementById('location1');
+ 
+  // const location1 = document.getElementById('location1');
   //  const location2 = document.getElementById('location2');
   //  const location3 = document.getElementById('location3');
   //  const location4 = document.getElementById('location4');
   //  const location5 = document.getElementById('location5'); 
   //  const location6 = document.getElementById('location6'); 
   const selectionFinal = new FormData(document.querySelector("form"));
-  //On fera une condition. Si l'input .value est égal à rien de tout, donc elle est vide et dans ce cas là, on ferait un epreventdefault. Et on arrete la soumission sinon on continue et sera envoyé au serveur. 
-
+ 
+    
   if (first.value == "") {   
     myError.innerHTML = "Veuillez entrer 2 lettres minimum pour le champ du prenom."; 
     myError.style.color = 'red';
@@ -126,30 +126,37 @@ reserve.addEventListener('submit',function(e) {
     document.getElementById("errorMessage").style.visibility = "hidden";
     return true;
   } 
-//   else
-//    {
-// alert('Merci! Votre réservation a été reçue.');
-//     } 
 
  
+});
+// 1ére méthode 
+
+const close = document.getElementById('close');
+const bground = document.getElementById('bground');
+ 
+close.addEventListener("click", () => {
+  if(getComputedStyle(bground).display != "none"){
+   bground.style.display = "none";
+  }    
+})
   
-//     if (erreur) {
-//       e.preventDefault();
-//      document.getElementById("erreur").innerHTML = erreur;
-//       return false;
-//     }  else {
-//     // alert('Merci! Votre réservation a été reçue.');
-//     } 
+
+// 2émé méthode -on click 
+
+// const close = document.getElementById("close"); 
+// const bground = document.getElementById('bground');
+
+// function closeM(){
+//  if(getComputedStyle( bground).display != "none"){
+//    bground.style.display = "none";
+//   }   
+// };
+// close.onclick = closeM;
 
 
- });
 
 
 
-// validation input check
-
-
-//function fermeture modal
 
 
 
